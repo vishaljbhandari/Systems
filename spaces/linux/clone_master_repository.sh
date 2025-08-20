18e0d957124e96890bce5b9bded56cd9da17d6b3
Vishal Bhandari <vishaljbhandari@gmail.com>
2025-08-20 16:19:01 +0530

    Update clone_master_repository.sh

--- spaces/linux/clone_master_repository.sh
+++ spaces/linux/clone_master_repository.sh
@@ -3,59 +3,67 @@ CURRENT_DIR='pwd'
 mkdir $REPO_HOME
 cd $REPO_HOME
 
-echo -n "Cloning Linux Space Repository"
-git clone git@github.com:vishaljbhandari/LinuxSpace.git
-
-echo -n "Cloning LEARN CPP Repository"
-git clone git@github.com:vishaljbhandari/learn_cpp.git
-
-echo -n "Cloning CPP AI Repository"
-git clone git@github.com:vishaljbhandari/CppAI.git
-
-echo -n "Cloning Environment Box Repository"
-git clone git@github.com:vishaljbhandari/EnvironmentBox.git
-
-echo -n "Cloning LEARN AI-ML Repository"
-git clone git@github.com:vishaljbhandari/LearnAI-ML.git
-
-echo -n "Cloning CLASSES CPP Repository"
-git clone git@github.com:vishaljbhandari/ClassesCpp.git
-
-echo -n "Cloning UNREAL ENGINE CPP Repository"
-git clone git@github.com:vishaljbhandari/UnrealEngineCpp.git
-
-echo -n "Cloning LEARN LINUX Repository"
-git clone git@github.com:vishaljbhandari/learn_linux.git
-
-echo -n "Cloning LEARN Java Repository"
-git clone git@github.com:vishaljbhandari/learn_java.git
-
-echo -n "Cloning LEARN C Repository"
-git clone git@github.com:vishaljbhandari/learn_c.git
-
-echo -n "Cloning LEARN Scripts Repository"
-git clone git@github.com:vishaljbhandari/learn_scripts.git
-
-echo -n "Cloning System Scripts C Repository"
-git clone git@github.com:vishaljbhandari/system_scripts.git
+echo -n "Cloning Learn Cpp Repository"
+git clone git@github.com:vishaljbhandari/LearnCpp.git
 
 echo -n "Cloning Projects Repository"
-git@github.com:vishaljbhandari/Projects.git
+git clone git@github.com:vishaljbhandari/Projects.git
 
-echo -n "Cloning Simple Examples Repository"
-git clone git@github.com:vishaljbhandari/simple_examples.git
+echo -n "Cloning System Repository"
+git clone git@github.com:vishaljbhandari/Systems.git
 
-echo -n "Cloning Workspace Repository"
-git clone git@github.com:vishaljbhandari/workspace.git
+echo -n "Cloning Learn SQL Repository"
+git clone git@github.com:vishaljbhandari/LearnSQL.git
 
 echo -n "Cloning Classes CPP Repository"
-git clone git@github.com:vishaljbhandari/classes_cpp.git
+git clone git@github.com:vishaljbhandari/ClassesCpp.git
 
-echo -n "Cloning System Scripts Repository"
-git clone git@github.com:vishaljbhandari/SystemScripts.git
+echo -n "Cloning Learn Cpp MFC  Repository"
+git clone git@github.com:vishaljbhandari/LearnCppMFC.git
 
+echo -n "Cloning Learn Script Repository"
+git clone git@github.com:vishaljbhandari/LearnScripts.git
+
+echo -n "Cloning Learn AI-ML Repository"
+git clone git@github.com:vishaljbhandari/LearnAI-ML.git
+
+echo -n "Cloning UnReal Engine Cpp Repository"
+git clone git@github.com:vishaljbhandari/UnrealEngineCpp.git
+
+echo -n "Cloning Cpp AI Repository"
+git clone git@github.com:vishaljbhandari/CppAI.git
+
+echo -n "Cloning Laern CSharp Repository"
+git clone git@github.com:vishaljbhandari/LearnCSharp.git
+
+echo -n "Cloning FAMEO Repository"
+git clone git@github.com:vishaljbhandari/Fameo.git
+
+echo -n "Cloning Learn React JS Repository"
+git clone git@github.com:vishaljbhandari/LearnReactJS.git
+
+echo -n "Cloning Learn TypeScript Repository"
+git clone git@github.com:vishaljbhandari/LearnTypeScript.git
+
+echo -n "Cloning Learn Go Repository"
+git clone git@github.com:vishaljbhandari/LearnGo.git
+
+echo -n "Cloning Learn C Repository"
+git clone git@github.com:vishaljbhandari/LearnC.git
+
+echo -n "Cloning Learn Java Repository"
+git clone git@github.com:vishaljbhandari/LearnJava.git
+
+echo -n "Cloning Learn Linux Repository"
+git clone git@github.com:vishaljbhandari/LearnLinux.git
+
+echo -n "Cloning Learn QT Repository"
+git clone git@github.com:vishaljbhandari/LearnQT.git
+
+echo -n "Cloning Learn Rest Repository"
+git clone git@github.com:vishaljbhandari/LearnRest.git
 
 echo -n "Cloning Sample Examples Repository"
-git@github.com:vishaljbhandari/SampleExamples.git
+git clone git@github.com:vishaljbhandari/SampleExamples.git
 
 cd $CURRENT_DIR
